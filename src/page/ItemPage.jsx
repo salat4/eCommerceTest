8fab7ede91954a183135ddb3534d0b502e43141c
Dima Malik <45137601+salat4@users.noreply.github.com>
2023-03-02 16:10:01 +0200

    Update ItemPage.jsx

--- src/page/ItemPage.jsx
+++ src/page/ItemPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Footer } from '../components/Footer/Footer'
+import { Header } from '../components/Header/Header'
 import { Item } from '../components/Item/Item'
 export const ItemPage = () => {
   return (
     <div>
+      <Header/>
       <Item/>
+      <Footer/>
     </div>
   )
 }
